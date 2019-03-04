11b55763551a2cc97328227a335dd381681854dc
Lev# <mr.lmartirosyan888@gmail.com>
2019-03-04 20:24:07 +0400

    relise one

---
+++ NumbreNegativeOrPositive/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberNegativeOrPositive
+{
+    class Program
+    {
+        static void PositiveNegative(int operand)
+        {
+            operand = operand >> 31;
+            if (operand == 0)
+            {
+                Console.WriteLine("Number Positive");
+            }
+            else
+            {
+                Console.WriteLine("Number Negative");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write number for check Positive or Negative :");
+            int operand = Convert.ToInt32(Console.ReadLine());
+                PositiveNegative(operand);
+
+            Console.ReadKey();
+        }
+    }
+}
